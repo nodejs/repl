929374d8374e9216308bc53a2a29e8c500159f83
Gus Caplan <me@gus.host>
2018-05-22 17:45:57 -0500

    update highlighting to match node style

--- src/highlight.js
+++ src/highlight.js
@@ -1,9 +1,43 @@
 'use strict';
 
-const emphasize = require('emphasize/lib/core');
-const js = require('highlight.js/lib/languages/javascript');
+const emphasize = require('emphasize');
+const chalk = require('chalk');
 
-emphasize.registerLanguage('javascript', js);
+const sheet = {
+  'comment': chalk.gray,
+  'quote': chalk.gray,
+
+  'keyword': chalk.green,
+  'addition': chalk.green,
+
+  'number': chalk.yellow,
+  'string': chalk.green,
+  'meta meta-string': chalk.cyan,
+  'literal': chalk.cyan,
+  'doctag': chalk.cyan,
+  'regexp': chalk.cyan,
+
+  'attribute': undefined,
+  'attr': undefined,
+  'variable': chalk.yellow,
+  'template-variable': chalk.yellow,
+  'class title': chalk.yellow,
+  'type': chalk.yellow,
+
+  'symbol': chalk.magenta,
+  'bullet': chalk.magenta,
+  'subst': chalk.magenta,
+  'meta': chalk.magenta,
+  'meta keyword': chalk.magenta,
+  'link': chalk.magenta,
+
+  'built_in': chalk.red,
+  'deletion': chalk.red,
+
+  'emphasis': chalk.italic,
+  'strong': chalk.bold,
+  'formula': chalk.inverse,
+};
 
 module.exports = (s) =>
-  emphasize.highlight('js', s).value;
+  emphasize.highlight('js', s, sheet).value;
