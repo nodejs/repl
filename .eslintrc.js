311a2ced50e2d7d42093897e9fd0797809b7c5af
Gus Caplan <me@gus.host>
2019-06-22 13:50:38 -0500

    keep updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,11 @@
 'use strict';
 
 module.exports = {
-  extends: 'airbnb',
+  extends: 'airbnb-base',
   parser: 'babel-eslint',
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 2019,
     sourceType: 'script',
-    ecmaFeatures: {
-      experimentalObjectRestSpread: true,
-    },
   },
   env: {
     es6: true,
@@ -31,12 +28,10 @@ module.exports = {
     'prefer-const': ['error', { destructuring: 'all' }],
     'class-methods-use-this': 'off',
     'implicit-arrow-linebreak': 'off',
-    'react/no-this-in-sfc': 'off',
     'import/no-dynamic-require': 'off',
     'import/no-extraneous-dependencies': ['error', {
       devDependencies: true,
     }],
     'import/extensions': 'off',
-    'react/destructuring-assignment': 'off',
   },
 };
